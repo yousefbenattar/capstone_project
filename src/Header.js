5b1e863becb1663a025d7e44a7af5fb189d05ecf
Youssef Ben Attar <youssefdisvitas@gmail.com>
2025-07-25 19:20:44 +0100

    first update

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import logo from '../src/assets/logo.png'
+
+function Header() {
+    return (
+    <header
+    
+    >
+    <img src={logo} alt="logo little lemon"/>
+    </header>
+    );
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+    <nav>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+    </nav>);
+}
+
+export default Nav;
